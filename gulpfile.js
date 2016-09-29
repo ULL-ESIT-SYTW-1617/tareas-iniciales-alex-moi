dedc7ffb03c70c460850aadb8a0793ecf9522bef
alu0100767421 <alu0100767421@ull.edu.es>
2016-09-29 17:34:48 +0000

    tareas para la creacion de wiki

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,27 @@
 var gulp  = require('gulp');
 var shell = require('gulp-shell');
 
-gulp.task('deploy', function () {
-  return gulp.src('').pipe(shell(["./scripts/deploy-gitbook"]));
-});
 
 gulp.task('build', function() {
   return gulp.src('').pipe(shell(['./scripts/generate-gitbook']));
 });
+
+gulp.task('deploy', function () {
+  return gulp.src('').pipe(shell(["./scripts/deploy-gitbook"]));
+});
+
 
 
 /*gulp.task('prueba',function() {
    return gulp.src('').pipe(shell(['gitbook build `pwd` ./gh-pages'])); 
 });
 */
+
+
+gulp.task('wikibuild', function() {
+   return gulp.src('').pipe(shell(['./scripts/generate-wiki'])); 
+});
+
+gulp.task('wikideploy', function() {
+   return gulp.src('').pipe(shell(['./scripts/deploy-wiki'])); 
+});
