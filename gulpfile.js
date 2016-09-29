2b9eedc5686114186ce30ed34fa848feda2874bb
alu0100767421 <alu0100767421@ull.edu.es>
2016-09-29 17:19:56 +0000

    Haciendo prueba

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,16 @@
 var gulp  = require('gulp');
 var shell = require('gulp-shell');
 
-
 gulp.task('deploy', function () {
-  return gulp.src('').pipe(shell(["./scripts/losh deploy-gitbook"]));
+  return gulp.src('').pipe(shell(["./scripts/deploy-gitbook"]));
 });
 
 gulp.task('build', function() {
   return gulp.src('').pipe(shell(['./scripts/generate-gitbook']));
 });
+
+
+/*gulp.task('prueba',function() {
+   return gulp.src('').pipe(shell(['gitbook build `pwd` ./gh-pages'])); 
+});
+*/
